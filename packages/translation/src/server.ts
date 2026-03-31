d0556d4a6d8ecd2127b78c96c267b7d1627eeed2
selcukcukur <selcukcukur@outlook.com.tr>
2026-03-31 23:28:02 +0300

    feat(translation): re-export next-intl server utilities

---
+++ packages/translation/src/server.ts
@@ -0,0 +1,23 @@
+import {
+  getLocale,
+  getFormatter,
+  getNow,
+  getTimeZone,
+  getTranslations,
+  getExtracted,
+  getMessages,
+  setRequestLocale,
+  getRequestConfig
+} from "next-intl/server"
+
+export {
+  getLocale,
+  getFormatter,
+  getNow,
+  getTimeZone,
+  getTranslations,
+  getExtracted,
+  getMessages,
+  setRequestLocale,
+  getRequestConfig
+}
