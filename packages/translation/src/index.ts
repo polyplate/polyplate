483e11f225536a747b38f285cc6befd6f6db8f1f
selcukcukur <selcukcukur@outlook.com.tr>
2026-03-31 23:28:11 +0300

    feat(translation): re-export next-intl client utilities

---
+++ packages/translation/src/index.ts
@@ -0,0 +1,23 @@
+import {
+  useTranslations,
+  useLocale,
+  useNow,
+  useFormatter,
+  useMessages,
+  useTimeZone,
+  hasLocale,
+  useExtracted,
+  NextIntlClientProvider as TranslationProvider
+} from "next-intl"
+
+export {
+  useLocale,
+  useTranslations,
+  useNow,
+  useFormatter,
+  useMessages,
+  useTimeZone,
+  useExtracted,
+  hasLocale,
+  TranslationProvider
+}
